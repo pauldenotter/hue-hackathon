a607571d3f92d9626b3c9dfe6e82ddc9028aefbe
Paul den Otter <pauldenotter@gmail.com>
2015-01-31 15:55:59 +0100

    Added part of algorithm

--- server/lib/sound.js
+++ server/lib/sound.js
@@ -1,4 +1,5 @@
-var clients = [];
+var clients = [],
+	buffer = [];
 
 module.exports = function(client) {
 	console.log('Client connected ');
@@ -17,9 +18,24 @@ module.exports = function(client) {
 function receive(buffer) {
 	var split = buffer.toString().split(';'),
 		data = {
+			ts: new Date(),
 			avg: parseFloat(split[0], 10),
 			peak: parseFloat(split[1], 10)
 		};
 
 	console.log(data);
 }
+
+setInterval(function() {
+	var bufferContents = buffer,
+		avg;
+	buffer = [];
+
+	avg = bufferContents.map(function(data) {
+		return data.avg;
+	}).reduce(function(a, b) {
+		return a + b;
+	}) / bufferContents.length;
+
+	console.log(avg);
+}, 4000);
