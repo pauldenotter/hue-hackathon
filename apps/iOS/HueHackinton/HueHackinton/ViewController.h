b3a32b57944d32b858cd8b131134d9b332cf9e02
Glenn Tillemans <glenn.tillemans@magneds.com>
2015-01-31 13:10:27 +0100

    init

---
+++ apps/iOS/HueHackinton/HueHackinton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HueHackinton
+//
+//  Created by Glenn Tillemans on 31-01-15.
+//  Copyright (c) 2015 Magneds B.V. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ apps/iOS/HueHackinton/HueHackinton/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HueHackinton
+//
+//  Created by Glenn Tillemans on 31-01-15.
+//  Copyright (c) 2015 Magneds B.V. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
